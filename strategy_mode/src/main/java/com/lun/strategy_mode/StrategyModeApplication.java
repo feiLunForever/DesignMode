b4fc8706a483e7b841c2b5c0cfaf16db8373431c
lun <mengdingbuhui@163.com>
2019-05-12 22:03:06 +0800

    Initial commit

---
+++ strategy_mode/src/main/java/com/lun/strategy_mode/StrategyModeApplication.java
@@ -0,0 +1,13 @@
+package com.lun.strategy_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyModeApplication.class, args);
+    }
+
+}
