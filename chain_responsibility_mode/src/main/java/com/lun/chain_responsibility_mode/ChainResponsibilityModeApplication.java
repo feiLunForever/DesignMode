ddc80388356330fad3957ae6bbb2c8ee8835ba66
lun <mengdingbuhui@163.com>
2019-05-12 23:03:27 +0800

    责任链模式

---
+++ chain_responsibility_mode/src/main/java/com/lun/chain_responsibility_mode/ChainResponsibilityModeApplication.java
@@ -0,0 +1,13 @@
+package com.lun.chain_responsibility_mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainResponsibilityModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainResponsibilityModeApplication.class, args);
+    }
+
+}
